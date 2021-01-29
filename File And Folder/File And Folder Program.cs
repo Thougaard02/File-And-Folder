d1bce37d6e3eb84633b6d0354d2937a6809358c2
Rasmus <Rasmus@vinperlen.dk>
2021-01-29 23:41:11 +0100

    Add project files.

---
+++ File And Folder/File And Folder Program.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace File_And_Folder
+{
+    class File_And_Folder_Program
+    {
+        public void Menu()
+        {
+            //Used to see files in the path
+            string path = @".\";
+            string seachPattern = "*";
+
+            //Menu Layout
+            Console.WriteLine("====================================================");
+            Console.WriteLine();
+            Console.WriteLine("               H1 Queue Operation Menu              ");
+            Console.WriteLine();
+            Console.WriteLine("====================================================");
+            Console.WriteLine("1. Add File");
+            Console.WriteLine("2. Delete File");
+            Console.WriteLine("3. Display Files");
+            Console.WriteLine("4. Add Folder");
+            Console.WriteLine("5. Search File");
+            Console.WriteLine("6. Exit");
+
+            int userOption = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(userOption);
+
+            //Could be change to a switch statement!!!!
+            //If userOption is equal it will run the method in the statement
+            if (userOption == 1)
+            {
+                Console.Clear();
+                AddFile();
+            }
+            else if (userOption == 2)
+            {
+                Console.Clear();
+                DeleteFile(path, seachPattern);
+            }
+            else if (userOption == 3)
+            {
+                Console.Clear();
+                ShowFiles(path, seachPattern);
+            }
+            else if (userOption == 4)
+            {
+                Console.Clear();
+                CreateFolder(path);
+            }
+            else if (userOption == 5)
+            {
+                Console.Clear();
+                ShowFiles(path);
+            }
+            else if (userOption == 6)
+            {
+                Environment.Exit(0);
+            }
+        }
+        #region Methodes
+        //Creates a file
+        private void CreateFile(string path)
+        {
+            File.Create(path);
+        }
+        //Creates a folder
+        private static void CreateDirectory(string path)
+        {
+            Directory.CreateDirectory(path);
+            Console.WriteLine("Directory created!");
+        }
+        //Deletes a file
+        private static void DeleteFil(string path)
+        {
+            File.Delete(path);
+            Console.WriteLine("File has been deleted!");
+        }
+        //Deletes a folder
+        private static void DeleteFolder(string path, bool recursive)
+        {
+            Directory.Delete(path, recursive);
+            Console.WriteLine("Folder has been deleted!");
+        }
+        //Printes the file the in the path
+        private static void GetFiles(string path, string searchPattern, SearchOption searchOption)
+        {
+            string[] files = Directory.GetFiles(path, searchPattern, searchOption);
+            for (int i = 0; i < files.Length; i++)
+            {
+                Console.WriteLine(files[i]);
+            }
+        }
+        #endregion
+        #region Option Methodes
+        //Creates a file
+        private void AddFile()
+        {
+            try
+            {
+                Console.WriteLine("Type your filename");
+                string userInput = Console.ReadLine();
+                if (!File.Exists($@".\{userInput}.txt"))
+                {
+                    Console.WriteLine("does not exists");
+                    CreateFile($"{userInput}.txt");
+                    Console.WriteLine("File has been created!");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    Menu();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("File already exists!");
+                    Thread.Sleep(500);
+                    Console.Clear();
+                    Menu();
+                }
+            }
+            catch (Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("Wrong input");
+                Thread.Sleep(500);
+                Console.Clear();
+                Menu();
+            }
+        }
+        //Deletes a file
+        private void DeleteFile(string path, string searchPattern)
+        {
+            try
+            {
+                Console.WriteLine("All your files can be seen under!\n");
+                GetFiles(path, searchPattern, SearchOption.AllDirectories);
+                Console.WriteLine("\nName the file");
+                Console.WriteLine("TYPE THE FILENAME you want to delete!!");
+                string userInput = Console.ReadLine();
+                if (!File.Exists($@".\{userInput}.txt"))
+                {
+                    Console.WriteLine("File doesn't exists");
+                    Console.ReadKey();
+                    Console.Clear();
+                    Menu();
+                }
+                else
+                {
+                    DeleteFil($@".\{userInput}.txt");
+                    Console.WriteLine($"File has been deleted {$@"{userInput}.txt"}!");
+                    Console.ReadKey();
+                    Console.Clear();
+                    Menu();
+                }
+            }
+            catch (Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("Wrong input");
+                Thread.Sleep(500);
+                Console.Clear();
+                Menu();
+            }
+        }
+        //Printes the all file the in the path
+        private void ShowFiles(string path, string searchPattern)
+        {
+            try
+            {
+                GetFiles(path, searchPattern, SearchOption.AllDirectories);
+                Console.WriteLine("\nPress any key to return to menu");
+                Console.ReadKey();
+                Console.Clear();
+                Menu();
+            }
+            catch (Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("Wrong input");
+                Thread.Sleep(500);
+                Console.Clear();
+                Menu();
+            }
+        }
+        //Creates a folder
+        private void CreateFolder(string path)
+        {
+            try
+            {
+                Console.WriteLine("Type your folder name");
+                string userInput = Console.ReadLine();
+                CreateDirectory($"{path}\\{userInput}");
+                Console.Clear();
+                Menu();
+            }
+            catch (Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("Wrong input");
+                Thread.Sleep(500);
+                Console.Clear();
+                Menu();
+            }
+        }
+        //Printes the file depends on the userinput in the path
+        private void ShowFiles(string path)
+        {
+            try
+            {
+                Console.WriteLine("Type the file format you want to search after!");
+                string userInput = $"*{Console.ReadLine()}";
+                GetFiles(path, userInput, SearchOption.AllDirectories);
+                Console.WriteLine("\nPress any key to return to menu");
+                Console.ReadKey();
+                Console.Clear();
+                Menu();
+            }
+            catch (Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("Wrong input");
+                Thread.Sleep(500);
+                Console.Clear();
+                Menu();
+            }
+        }
+        #endregion
+    }
+}
---
+++ File And Folder/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.AccessControl;
+
+namespace File_And_Folder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Runs the class File_And_Folder_Program
+            File_And_Folder_Program program = new File_And_Folder_Program();
+            program.Menu();
+
+            #region File / Directory
+            //Path
+            //string path = @"Droids\protocol";
+
+            #region Opg 1
+            //Files path and writes inside the file
+            //string fileName = $"{path}\\protocol.txt";
+            //string contents = "Hello World!";
+
+            //WriteAllText(fileName, contents);
+            #endregion
+
+            #region Opg 2
+            //Read the text inside file.
+            //ReadAllText(path);
+            #endregion
+
+            #region Opg 3
+            ////Deletes file
+            //DeleteFile(path);
+            #endregion
+
+            #region Opg 4
+            //Create a directory
+            //CreateDirectory(path);
+            #endregion
+
+            #region Opg 5
+            //Deletes folder
+            //DeleteFolder(path, true);
+
+            #endregion
+
+            #region Opg 6
+
+            #endregion
+            //Enumerating files
+            //string seachPattern = "*";
+            //GetFiles(path, seachPattern, SearchOption.AllDirectories);
+            #endregion
+            #region Stream
+            //string filePath = @"Movies.txt";
+            //Create file
+            //CreateFile(filePath);
+
+            //Writing to .txt
+            //string text = "Star wars\nThe Impire Strickes Back\nReturn of the jedi\n";
+            //WriteToFile(filePath, text);
+
+            //Writing a collection
+            //CollectionWrite(filePath);
+
+            //Reads the whole file becuase the function "ReadToEnd()"
+            //ReadFile(filePath);
+
+            //Append text
+            //AppendText(filePath, text);
+
+            #endregion
+            Console.ReadKey();
+        }
+
+
+        #region File / Directory
+
+        //public static void WriteAllText(string fileName, string contens)
+        //{
+        //    File.WriteAllText(fileName, contens);
+        //    Console.WriteLine("File created!");
+        //}
+        //public static void ReadAllText(string path)
+        //{
+        //    string readFile = File.ReadAllText(path);
+        //    Console.WriteLine(readFile);
+        //}
+        //public static void CreateDirectory(string path)
+        //{
+        //    Directory.CreateDirectory(path);
+        //    Console.WriteLine("Directory created!");
+        //}
+
+        //public static void DeleteFile(string path)
+        //{
+        //    File.Delete(path);
+        //    Console.WriteLine("File has been deleted!");
+        //}
+        //public static void DeleteFolder(string path, bool recursive)
+        //{
+        //    Directory.Delete(path, recursive);
+        //    Console.WriteLine("Folder has been deleted!");
+        //}
+
+        public static void GetFiles(string path, string searchPattern, SearchOption searchOption)
+        {
+            string[] files = Directory.GetFiles(path, searchPattern, searchOption);
+            for (int i = 0; i < files.Length; i++)
+            {
+                Console.WriteLine(files[i]);
+            }
+        }
+        #endregion
+        #region Stream
+
+        //static void CreateFile(string filePath)
+        //{
+        //    FileStream create = new FileStream(filePath, FileMode.Create);
+
+        //    Console.WriteLine("File has been created!");
+        //}
+        //static void ReadFile(string filePath)
+        //{
+        //    FileStream file = new FileStream(filePath, FileMode.Open);
+        //    StreamReader reader = new StreamReader(file);
+        //    Console.WriteLine(reader.ReadToEnd());
+        //    file.Close();
+        //}
+
+        //static void WriteToFile(string filePath, string text)
+        //{
+        //    using (StreamWriter writer = new StreamWriter(new FileStream(filePath, FileMode.Create)))
+        //    {
+        //        writer.WriteLine(text);
+        //    }
+
+        //    //File.AppendAllText(filePath, text);
+        //    Console.WriteLine("Writing done");
+        //}
+        //static void CollectionWrite(string filePath)
+        //{
+        //    string[] jedis = new[] { "Luke Skywalker", "Yoda", "Mace Windu" };
+        //    foreach (string jedi in jedis)
+        //    {
+        //        File.AppendAllText(filePath, jedi);
+        //    }
+        //}
+        ////static void CollectionWrite(string filePath)
+        ////{
+        ////    string[] actors = new[] { "Mark Hamill\n", "Harrision Ford\n", "Carrie Fisher\n" };
+        ////    foreach (string actor in actors)
+        ////    {
+        ////        File.AppendAllText(filePath, actor);
+        ////    }
+        ////}
+        //static void AppendText(string filePath, string text)
+        //{
+        //    File.AppendAllText(filePath, text);
+        //}
+        #endregion
+    }
+}
